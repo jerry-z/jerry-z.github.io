42415122963ddb754323a7391dfde435f3aa67fb
jz2966@columbia.edu <jerry-z>
2019-04-10 01:12:17 +0000

    colab -> github

--- RNN-text-classification/index.js
+++ RNN-text-classification/index.js
@@ -12,7 +12,7 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'  
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.',  
   'example4':
       'Iago. It is meerly a Lust of the blood, and a permission of the will.'  
 };
